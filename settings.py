ad8a130199cb65b76ad554745d5c3e8e4e18881b
Gibram Favela <Gibram_Favela1@baylor.edu>
2021-03-08 12:46:14 -0600

    nineteenth commit

--- settings.py
+++ settings.py
@@ -14,6 +14,9 @@ class Settings:
         self.bullet_height = 15
         self.bullet_color = (60, 60, 60)
         self.bullets_allowed = 3
+        # Alien settings
+        self.alien_speed = 1.0
+
 
 
 
